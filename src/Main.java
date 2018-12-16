ef08d188a27d93e746d3039027246450be9f48ef
shodgearo <shodkvest@gmail.com>
2018-12-16 19:35:51 +0600

    Update git ignore and finish this program

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,17 @@
 // Утилита командной строки, которая выводит расширение файла.
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        String s = "";
 
+        Scanner scan = new Scanner(System.in);
+
+        s = scan.next();
+
+        String[] mas = s.split("\\.");
+
+        System.out.println(mas[mas.length - 1]);
     }
 }
